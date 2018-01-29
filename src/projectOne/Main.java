fbf30e4171ea0f0011516f20865e86bd972c1ebb
Siavash <Siavash_N@outlook.com>
2018-01-29 01:53:00 -0500

    Added the FileManager class - tested

--- src/projectOne/Main.java
+++ src/projectOne/Main.java
@@ -1,10 +1,18 @@
 package projectOne;
 
+import java.util.ArrayList;
+import projectOne.file.FileManager;
+
 public class Main {
+	static final FileManager fm = FileManager.getCurrent();
+	static final String fileName = "Sia.txt";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		System.out.println("Write file result: " + fm.appendFile("Hi\r\nHow're you?", fileName));
+		
+		ArrayList<String> lines = fm.readFile(fileName,20);
+		System.out.println("Lines: " + lines.size());
+		System.out.println("Last line: " + lines.get(lines.size()-1));
 	}
 
 }
