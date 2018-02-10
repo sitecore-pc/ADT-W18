272ea7284253b9038a0b3ef45d1510d8ae1de9d0
Siavash <Siavash_N@outlook.com>
2018-02-09 22:06:12 -0500

    Added two packages projectOne.common and projectOne.models -- Added the Tuple model -- Added the Iconvertable interface to implement the parse() and toString() -- Tuple is implementing the IConvertable -- added the required common utilities -- added the required tests for the Tuple and conversions in Main

--- src/projectOne/Main.java
+++ src/projectOne/Main.java
@@ -1,18 +1,58 @@
 package projectOne;
 
 import java.util.ArrayList;
-import projectOne.file.FileManager;
+import java.util.Iterator;
+
+import projectOne.file.*;
+import projectOne.models.*;
 
 public class Main {
 	static final FileManager fm = FileManager.getCurrent();
 	static final String fileName = "Sia.txt";
 
 	public static void main(String[] args) {
-		System.out.println("Write file result: " + fm.appendFile("Hi\r\nHow're you?", fileName));
 		
+		//FileManager test
+		System.out.println("Write file result: " + fm.appendFile("Hi\r\nHow're you?", fileName));
 		ArrayList<String> lines = fm.readFile(fileName,20);
 		System.out.println("Lines: " + lines.size());
 		System.out.println("Last line: " + lines.get(lines.size()-1));
+		
+		
+		//Tuple model test
+		//Filling a new list of Tuples
+		ArrayList<Tuple> students = new ArrayList<Tuple>();
+		try {
+			students.add(new Tuple(11111,"John", "Smith", 111,222,123456789,"3340, Maisonneuve, Montreal, QC"));
+			students.add(new Tuple(22222222,"John", "Doe", 333,333,987654321,"2100, Maisonneuve, Montreal, QC"));
+			students.add(new Tuple(12345678,"Jane", "Doe", 222,111,0,"4850, Cote-des-Neiges H3V1G5 Apt. 1106, Montreal, QC34567"));
+			students.add(new Tuple(88888888,"Jasonsonny", "Jackson890", 8,0,999999999,"4858, Cote-des-Neiges H3V1G8 Apt. 803, Montreal, QC345678"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+
+		//Converting the list of Tuples to string and printing
+		ArrayList<String> studentStrings = new ArrayList<String>();
+		for (Iterator<Tuple> studentsIterator = students.iterator(); studentsIterator.hasNext();) {
+			Tuple student = studentsIterator.next();
+			String stdString = student.toString();
+			studentStrings.add(stdString);
+			System.out.println(stdString);
+		}
+		
+		//Parsing back the string list to Tuple
+		ArrayList<Tuple> reparsedStudents = new ArrayList<Tuple>();
+		for (Iterator<String> studentsIterator = studentStrings.iterator(); studentsIterator.hasNext();) {
+			String stdString = studentsIterator.next();
+			reparsedStudents.add(new Tuple(stdString));
+		}
+		
+		//Printing the parsed list again
+		for (Iterator<Tuple> studentsIterator = reparsedStudents.iterator(); studentsIterator.hasNext();) {
+			Tuple student = studentsIterator.next();
+			System.out.println(student.toString());
+		}
 	}
 
 }
