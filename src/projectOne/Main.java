8ed1d59c26e4f450010a11dd38584cd07ee2f096
Siavash <Siavash_N@outlook.com>
2018-03-26 00:37:39 -0400

    Removed the old FileManager -- Removed a few extra variables and imports

--- src/projectOne/Main.java
+++ src/projectOne/Main.java
@@ -1,9 +1,6 @@
 package projectOne;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.math.*;
 import projectOne.bagDifference.BagDifference;
 import projectOne.common.Parameters;
 import projectOne.file.*;
@@ -28,7 +25,7 @@ public class Main {
 		long ioCountTPMMS = FileManagerV2.getCounter();
 		
 		System.out.print("BD: Bag Difference... ");
-		long resultSize = BagDifference.comparator(bag1,bag2);
+		/*long resultSize = */BagDifference.comparator(bag1,bag2);
 		
 		long endTime = System.nanoTime();
 		long ioCountTotal = FileManagerV2.getCounter();
@@ -121,61 +118,5 @@ public class Main {
 		
 		f3.deleteFile();
 		System.out.println("Number of lines in F2.txt: " + f2.getTotalNumberOfRows());
-	}
-
-	public static void TestFileAndTuple() {
-		final FileManager fm = FileManager.getCurrent();
-		
-		// FileManager test
-		// System.out.println("Write file result: " + fm.appendFile("Hi\r\nHow're you?",
-		// fileName));
-		// ArrayList<String> lines = fm.readFile(fileName,20);
-		// System.out.println("Lines: " + lines.size());
-		// System.out.println("Last line: " + lines.get(lines.size()-1));
-
-		// Tuple model test
-		// Filling a new list of Tuples
-		ArrayList<Tuple> students = new ArrayList<Tuple>();
-		try {
-			students.add(new Tuple(11111, "John", "Smith", 111, 222, 123456789, "3340, Maisonneuve, Montreal, QC"));
-			students.add(new Tuple(22222222, "John", "Doe", 333, 333, 987654321, "2100, Maisonneuve, Montreal, QC"));
-			students.add(new Tuple(12345678, "Jane", "Doe", 222, 111, 0,
-					"4850, Cote-des-Neiges H3V1G5 Apt. 1106, Montreal, QC34567"));
-			students.add(new Tuple(88888888, "Jasonsonny", "Jackson890", 8, 0, 999999999,
-					"4858, Cote-des-Neiges H3V1G8 Apt. 803, Montreal, QC345678"));
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		}
-
-		// Converting the list of Tuples to string and printing
-		ArrayList<String> studentStrings = new ArrayList<String>();
-		for (Iterator<Tuple> studentsIterator = students.iterator(); studentsIterator.hasNext();) {
-			Tuple student = studentsIterator.next();
-			String stdString = student.toString();
-			studentStrings.add(stdString);
-			System.out.println(stdString);
-		}
-
-		// Parsing back the string list to Tuple
-		ArrayList<Tuple> reparsedStudents = new ArrayList<Tuple>();
-		for (Iterator<String> studentsIterator = studentStrings.iterator(); studentsIterator.hasNext();) {
-			String stdString = studentsIterator.next();
-			//1st option
-			Tuple t = new Tuple(stdString);
-			//2nd option
-			Tuple t2 = new Tuple();
-			t2.parse(stdString);
-			//3rd option
-			Tuple t3 = Tuple.Parse(stdString);
-			
-			reparsedStudents.add(t);
-		}
-
-		// Printing the parsed list again
-		for (Iterator<Tuple> studentsIterator = reparsedStudents.iterator(); studentsIterator.hasNext();) {
-			Tuple student = studentsIterator.next();
-			System.out.println(student.toString());
-		}
 	}
 }
